a1cca275ad27fe92aea9bda521e91045439c5cc1
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-25 16:20:34 -0700

    Add completed exericises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(message, num) {
+  let completeMessage = message;
+  if (num === 0 || message === "") {
+    completeMessage = "";
+  } else if (num < 0) {
+    completeMessage = "ERROR";
+  } else if (num == "number") {
+    num = number;
+  }
+  for (i = 0; i < num - 1; i++) {
+    completeMessage += message;
+  }
+  return completeMessage;
 };
 
 // Do not edit below this line
