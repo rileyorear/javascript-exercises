a91949f51b7b4ea5494a05ce1408bad78b761f71
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-26 13:10:50 -0700

    Add completed exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    for (; num1 <= num2; num1++) {
+      sum += num1;
+    }
+  } else if (num2 < num1) {
+    for (; num2 <= num1; num2++) {
+      sum += num2;
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+  let leapYear = false;
+  if (year % 4 === 0) {
+    leapYear = true;
+    if (year % 100 === 0) {
+      leapYear = false;
+      if (year % 400 === 0) {
+        leapYear = true;
+      }
+    }
+  }
+  return leapYear;
+}
+  
 
 // Do not edit below this line
 module.exports = leapYears;
